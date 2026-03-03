f29879846384ee01e96a64fc647a3800e63b3266
fnajeraisw-hash <f.najera.isw@unipolidgo.edu.mx>
2026-03-03 10:36:03 -0600

    Debug option added

--- backend/routes/aparments.routes.js
+++ backend/routes/aparments.routes.js
@@ -8,24 +8,27 @@ const router = Router();
 // GET all apartments
 router.get("/apartments", authMiddleware, async (req, res) => {
   try {
-    const ownerId = req.user.id; // <-- from the JWT
+    const ownerId = req.user.id;
 
     const result = await pool.query(
-      `SELECT 
-    a.*,
-    rc.amount,
-    t.name AS tenant_name
-FROM apartments a
-LEFT JOIN rentalcontracts rc 
-    ON rc.apartment_id = a.id
-LEFT JOIN tenants t 
-    ON rc.tenant_id = t.id
-WHERE a.ownerid = $1;`,
+      `
+      SELECT 
+        a.*,
+        rc.amount,
+        t.name AS tenant_name
+      FROM apartments a
+      LEFT JOIN rentalcontracts rc 
+        ON rc.apartment_id = a.id
+      LEFT JOIN tenants t 
+        ON rc.tenant_id = t.id
+      WHERE a.ownerid = $1;
+      `,
       [ownerId]
     );
 
     res.json(result.rows);
   } catch (err) {
+    console.error(err); // important for debugging
     res.status(500).json({ error: "Error fetching apartments" });
   }
 });
